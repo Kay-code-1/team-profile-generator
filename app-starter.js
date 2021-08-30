4389e7e7ad7b804d4e996eb2f11b35f4c2175fd3
kalyani joshi <kalyani121@gmail.com>
2021-08-30 18:40:27 -0400

    added prompt for manager and validation for phone and email

--- app-starter.js
+++ app-starter.js
@@ -14,28 +14,67 @@ const render = require("./lib/htmlRenderer");
 // const render = require("./lib/page-template.js");
 
 
-const teamMembers = [];
+const teamMembers = ["Manager", "Engineer", "Intern"];
 // Create an id array to store the ids.
 // This array will be used to check the potential duplicate id newly entered by user
-const idArray = [];
+
+const idArray = [101, 102, 103, 104, 105, 106, 107, 108, 109, 110];
 
 function appMenu() {
-
   function createManager() {
     console.log("Please build your team");
     inquirer.prompt([
-      //
-      // YOUR CODE HERE:
-      // CREATE OBJECTS OF QUESTIONS HERE FOR MANAGER
-      // Strongly recommend to add validate property function for id and email
-      //
+      //Question Objects for manager
+      { 
+        type: "input",
+        name: "managerName",
+        message: "Enter your name.",
+      },
+      {
+        type: "list",
+        name: "managerID",
+        message: "Select your Employee ID.",
+        choices: idArray,
+      },
+      {
+        type: "input",
+        name: "managerEmail",
+        message: "Enter your Email ID.",
+        default: () => {},
+        //Validation for email ID with Regex
+        validate: (email) => {
+          valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+            if (valid) {
+              console.log("Correct email format!");
+                return true;
+            } else {
+                console.log("Please enter a valid email")
+                return false;
+            }
+        }
+      },
+      {
+        type: "input",
+        name: "managerOfficeNumber",
+        message: "Enter your Office number",
+        //Validate if office number has only digits
+        validate: (officeNum) => {
+          if(officeNum === NaN) {
+            console.log("Enter a valid phone number!");
+          } else {
+            console.log("Correct phone number!");
+            return true;
+          }
+        },
+      },
     ]).then(answers => {
-      const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
-      teamMembers.push(manager);
-      idArray.push(answers.managerId);
-      createTeam();
-    });
-  }
+        const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
+        teamMembers.push(manager);
+        idArray.push(answers.managerId);
+        createTeam();
+      });
+    }
+   }
 
   function createTeam() {
 
